2172089992122428feadb372297dca9abcebc3be
mistRain <mahdimousavi40@gmail.com>
2022-05-23 00:26:00 +0430

    Create PageHome template name and posts by its id

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -1,10 +1,34 @@
 <template>
-<div></div>
+<div>Hello from pageHome</div>
+<div v-for="thread in threads" :key="thread.id">
+    <h2>{{ thread.title }}</h2>
+    <div v-for="postId in thread.posts" :key="postId">
+        <p>{{ userById(postById(postId).userId).name }}</p>
+        <p>{{ postById(postId).text }}</p>
+    </div>
+</div>
 </template>
 
 <script>
-export default {
+import dataSource from '@/data.json'
+console.log(dataSource)
 
+export default {
+  data () {
+    return {
+      threads: dataSource.threads,
+      posts: dataSource.posts,
+      users: dataSource.users
+    }
+  },
+  methods: {
+    postById (postId) {
+      return this.posts.find(post => post.id === postId)
+    },
+    userById (userId) {
+      return this.users.find(user => user.id === userId)
+    }
+  }
 }
 </script>
 
