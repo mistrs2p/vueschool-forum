7a87bb531723261ac883ccb4a7577bff81b78c96
mistRain <mahdimousavi40@gmail.com>
2022-05-24 16:51:34 +0430

    Page Not Found Modified

--- src/components/PageNotFound.vue
+++ src/components/PageNotFound.vue
@@ -1,5 +1,8 @@
 <template>
-  <h1>Not Found</h1>
+<div class="col-full text-center">
+    <h1>Not Found</h1>
+    <router-link :to="{name: 'Home'}" >Read some cool threads</router-link>
+</div>
 </template>
 
 <script>
