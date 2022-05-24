3bdc50ef95c6afb630e856abeebde91aaa030ea8
mistRain <mahdimousavi40@gmail.com>
2022-05-24 16:26:57 +0430

    ThreadList component Added to show the list thread and PageHome Refactored

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -1,66 +1,16 @@
 <template>
-<div
-    v-for="thread in threads"
-    :key="thread.id"
-    class="col-large push-top"
->
-          <h1>{{ thread.title}}</h1>
-
-          <div
-            v-for="postId in thread.posts"
-            :key="postId"
-            class="post-list"
-          >
-
-              <div class="post">
-
-                  <div class="user-info">
-                      <a href="#" class="user-name">{{ userById(postById(postId).userId).name }}</a>
-
-                      <a href="#">
-                          <img class="avatar-large" :src="userById(postById(postId).userId).avatar" alt="">
-                      </a>
-
-                      <p class="desktop-only text-small">107 posts</p>
-
-                  </div>
-
-                  <div class="post-content">
-                      <div>
-                        <p>
-                          {{ postById(postId).text }}
-                        </p>
-                      </div>
-                  </div>
-
-                  <div class="post-date text-faded">
-                      {{postById(postId).publishedAt}}
-                  </div>
-
-              </div>
-          </div>
-      </div>
+<h1>Welcome to the Forum</h1>
+<ThreadList />
 </template>
 
 <script>
 import dataSource from '@/data.json'
+import ThreadList from '@/components/ThreadList'
 console.log(dataSource)
 
 export default {
-  data () {
-    return {
-      threads: dataSource.threads,
-      posts: dataSource.posts,
-      users: dataSource.users
-    }
-  },
-  methods: {
-    postById (postId) {
-      return this.posts.find(post => post.id === postId)
-    },
-    userById (userId) {
-      return this.users.find(user => user.id === userId)
-    }
+  components: {
+    ThreadList
   }
 }
 </script>
