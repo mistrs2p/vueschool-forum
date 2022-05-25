0655d7716528580e67a63ddb2740ddc852eeea8b
mistRain <mahdimousavi40@gmail.com>
2022-05-25 17:07:24 +0430

    Add Post Code move to a new component to make it component base and config Set

---
+++ src/components/PostEditor.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="col-full">
+    <form @submit.prevent="save">
+      <div class="form-group">
+        <textarea
+          :value="text"
+          @input="text = $event.target.value"
+          name=""
+          id=""
+          cols="30"
+          rows="10"
+          class="form-input"
+        ></textarea>
+      </div>
+      <div class="form-actions">
+        <button class="btn-blue">Submit post</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      text: ''
+    }
+  },
+  methods: {
+    save () {
+      const postId = 'gggg' + Math.random()
+      const post = {
+        id: postId,
+        text: this.text,
+        publishedAt: Math.floor(Date.now() / 1000),
+        userId: 'rpbB8C6ifrYmNDufMERWfQUoa202'
+      }
+      this.$emit('save', { post })
+      //   this.posts.push(post)
+      //   this.thread.posts.push(postId)
+      //   console.log(post)
+      this.text = ''
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- src/pages/ThreadShow.vue
+++ src/pages/ThreadShow.vue
@@ -1,31 +1,15 @@
 <template>
-<!-- if an invalid id searched by user, here one of the solutions, add vif for thread -->
-  <div
-    v-if="thread"
-    class="col-large push-top"
-  >
+  <div class="col-large push-top">
     <h1>{{ thread.title }}</h1>
-    <PostList :posts="threadPosts"/>
-    <div class="col-full">
-      <form  @submit.prevent="addPost">
-        <div class="form-group">
-          <textarea :value="newPostText" @input="newPostText = $event.target.value" name="" id="" cols="30" rows="10" class="form-input"></textarea>
-        </div>
-        <div class="form-actions">
-          <button class="btn-blue">Submit post</button>
-        </div>
-      </form>
-    </div>
+    <PostList :posts="threadPosts" />
+    <PostEditor @save="addPost" />
   </div>
-  <!-- <div class="col-full text-center">
-    <h1>This Thread does not exist</h1>
-    <router-link :to="{name: 'Home'}" >Read some cool threads</router-link>
-  </div> -->
 </template>
 
 <script>
 import dataSource from '@/data.json'
 import PostList from '@/components/PostList'
+import PostEditor from '@/components/PostEditor'
 console.log(dataSource)
 
 export default {
@@ -36,44 +20,35 @@ export default {
     }
   },
   components: {
-    PostList
+    PostList,
+    PostEditor
   },
   data () {
     return {
       threads: dataSource.threads,
-      posts: dataSource.posts,
-      newPostText: ''
+      posts: dataSource.posts
     }
   },
   computed: {
     thread () {
-      // return this.threads.find(t => t.id === this.id)
-      return this.threads.find(t => t.id === this.$route.params.id)
+      return this.threads.find((t) => t.id === this.$route.params.id)
     },
     threadPosts () {
-      return this.posts.filter(post => post.threadId === this.id)
+      return this.posts.filter((post) => post.threadId === this.id)
     }
   },
   methods: {
-    addPost () {
-      const postId = 'gggg' + Math.random()
+    addPost (eventData) {
       const post = {
-        id: postId,
-        text: this.newPostText,
-        publishedAt: Math.floor(Date.now() / 1000),
-        threadId: this.id,
-        userId: 'rpbB8C6ifrYmNDufMERWfQUoa202'
+        ...eventData.post,
+        threadId: this.id
       }
       this.posts.push(post)
-      this.thread.posts.push(postId)
-      console.log(post)
-      this.newPostText = ''
+      this.thread.posts.push(post.id)
     }
   }
-
 }
 </script>
 
 <style scoped>
-
 </style>
