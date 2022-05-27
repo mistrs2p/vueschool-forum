640bf6341600703bb83b354eaec6227cf2d6a5ae
mistRain <mahdimousavi40@gmail.com>
2022-05-27 15:19:26 +0430

    CategoryList comp added and set

---
+++ src/components/CategoryList.vue
@@ -0,0 +1,33 @@
+<template>
+<ForumList
+    v-for="category in categories"
+    :key="category.id"
+    :forums="getForumsForCategory(category)"
+    :categorty-name="category.name"
+/>
+</template>
+
+<script>
+import ForumList from './ForumList.vue'
+import sourceData from '@/data.json'
+export default {
+  components: {
+    ForumList
+  },
+  props: {
+    categories: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    getForumsForCategory (category) {
+      return sourceData.forums.filter(forum => forum.categoryId === category.id)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
