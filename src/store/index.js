1735afa0b664a5cdf3835631125e3d050a563888
mistRain <mahdimousavi40@gmail.com>
2022-05-27 20:14:24 +0430

    Create post set to Vuex context

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,24 @@
 import { createStore } from 'vuex'
 import sourceData from '@/data'
 export default createStore({
-  state: sourceData
+  state: sourceData,
+  actions: {
+    createPost (context, post) {
+      post.id = 'gggg' + Math.random()
+      // Set the post
+      context.commit('setPost', { post })
+      // Append the post to the thread
+      context.commit('appendPostToThread', { postId: post.id, threadId: post.threadId })
+      this.thread.posts.push(post.id)
+    }
+  },
+  mutations: {
+    setPost (state, { post }) {
+      state.posts.push(post)
+    },
+    appendPostToThread (state, { postId, threadId }) {
+      const thread = state.threads.find(thread => thread.id === threadId)
+      thread.posts.push(postId)
+    }
+  }
 })
