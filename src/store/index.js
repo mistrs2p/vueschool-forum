ee0f0472eabb7337bc22636395227f44d1c73a00
mistRain <mahdimousavi40@gmail.com>
2022-06-01 20:05:29 +0430

    Store config for thread add Set

--- src/store/index.js
+++ src/store/index.js
@@ -41,6 +41,16 @@ export default createStore({
       commit('appendPostToThread', { postId: post.id, threadId: post.threadId })
       // this.thread.posts.push(post.id)
     },
+    createThread ({ commit, state, dispatch }, { title, text, forumId }) {
+      const id = 'gggg' + Math.random()
+      const userId = state.authId
+      const publishedAt = Math.floor(Date.now() / 1000)
+      const thread = { forumId, title, publishedAt, userId, id }
+      commit('setThread', { thread })
+      commit('appendThreadToUser', { userId, threadId: id })
+      commit('appendThreadToForum', { forumId, threadId: id })
+      dispatch('createPost', { text, threadId: id })
+    },
     updateUser ({ commit }, user) {
       commit('setUser', { user, userId: user.id })
     }
@@ -49,13 +59,27 @@ export default createStore({
     setPost (state, { post }) {
       state.posts.push(post)
     },
+    setThread (state, { thread }) {
+      state.threads.push(thread)
+    },
     setUser (state, { user, userId }) {
       const userIndex = state.users.findIndex(user => user.id === userId)
       state.users[userIndex] = user
     },
     appendPostToThread (state, { postId, threadId }) {
       const thread = state.threads.find(thread => thread.id === threadId)
+      thread.posts = thread.posts || []
       thread.posts.push(postId)
+    },
+    appendThreadToForum (state, { forumId, threadId }) {
+      const forum = state.forums.find(forum => forum.id === forumId)
+      forum.threads = forum.threads || []
+      forum.threads.push(threadId)
+    },
+    appendThreadToUser (state, { userId, threadId }) {
+      const user = state.users.find(user => user.id === userId)
+      user.posts = user.posts || []
+      user.posts.push(threadId)
     }
   }
 })
